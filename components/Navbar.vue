6f80fda88b9e945e10583c970f7a84ad0c83faed
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-02-01 18:07:18 -0800

    add magic link user system

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,41 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+const email = ref('')
+function showLoginForm() {
+  const form = document.querySelector('.form')
+  form?.classList.toggle('hidden')
+  form?.classList.toggle('flex')
+  form?.classList.toggle('flex-col')
+}
+
+async function handleLogin() {
+  if (user.value) {
+    const { error } = await client.auth.signOut()
+  } else {
+    const { error } = await client.auth.signInWithOtp({
+      email: email.value, options: {
+        emailRedirectTo: 'http://localhost:3000'
+      }
+    })
+  }
+}
+</script>
+
 <template>
-  <header class="w-full flex flex-initial justify-between h-16 items-center pt-8">
-    <p class="ml-4 flex items-center"><img src="NFL.svg" width="50" height="50" /> NFL Typer</p>
-    <div class="flex gap-4">
+  <header class="flex h-16 w-full flex-initial items-center justify-between pt-8">
+    <p class="ml-4 flex items-center">
+      <img src="NFL.svg" width="50" height="50" /> NFL Typer
+    </p>
+    <div class="flex gap-4 relative">
       <p>Your Scores</p>
-      <p class="mr-4">Sign In</p>
+      <button v-if="user" v-on:click="showLoginForm" class="mr-4">Log Out</button>
+      <button v-else v-on:click="showLoginForm" class="mr-4">Sign In</button>
+      <form class="bg-gray-500 form absolute hidden top-10 right-7" @submit.prevent="handleLogin">
+        <input class="p-2 bg-gray-500 mb-2" placeholder="Email" v-model="email" />
+        <input type="submit" class="text-sm m-1 p-2 bg-green-700 text-white font-bold rounded-md"
+          value="Send magic link" />
+      </form>
     </div>
   </header>
 </template>
--- utils/dbFiller.ts
+++ utils/dbFiller.ts
@@ -1,16 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
-import { writeFileSync } from "fs"
+import { createClient } from '@supabase/supabase-js'
+import { writeFileSync } from 'fs'
 
 // ran 1/26/2023, may need to run again at end of season if that matters
 // using supabase for now but there is a chance i switch to local
 export async function getRosters(): Promise<void> {
-
   // const supabase = createClient('https://qapykcdprhrersuvogia.supabase.co', '')
   for (let i = 33; i <= 34; i++) {
     const teamRes = await fetch(
       `https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${i}/roster`
-    );
-    const teamJson: ESPNResponse = await teamRes.json();
+    )
+    const teamJson: ESPNResponse = await teamRes.json()
     // const players: Player[] = []
     for (let j = 0; j <= 2; j++) {
       for (const player of teamJson.athletes[j].items) {
@@ -28,76 +27,76 @@ export async function getRosters(): Promise<void> {
           role: teamJson.athletes[j].position,
           position: player.position.name,
           image: player.headshot?.href,
-        };
+        }
         // players.push(athlete)
         const { error } = await supabase
-          .from("Player")
-          .upsert({...athlete}, {ignoreDuplicates: true});
+          .from('Player')
+          .upsert({ ...athlete }, { ignoreDuplicates: true })
       }
     }
     // writeFileSync("players.json", JSON.stringify(players))
   }
-  return;
+  return
 }
 
-getRosters();
+getRosters()
 
 export interface Player {
-  firstName: string;
-  lastName: string;
-  fullName: string;
-  state?: string;
-  college?: string;
-  collegeShort?: string;
-  city?: string;
-  country?: string;
-  debutYear?: number;
-  team: string;
-  role: string;
-  position: string;
-  image: string;
+  firstName: string
+  lastName: string
+  fullName: string
+  state?: string
+  college?: string
+  collegeShort?: string
+  city?: string
+  country?: string
+  debutYear?: number
+  team: string
+  role: string
+  position: string
+  image: string
 }
 
 interface ESPNResponse {
-  [key: string]: any;
-  athletes: Athlete[];
-  team: any;
+  [key: string]: any
+  athletes: Athlete[]
+  team: any
 }
 
 interface Team {
-  [key: string]: any;
-  displayName: string;
+  [key: string]: any
+  displayName: string
 }
 
 interface Athlete {
-  position: string;
-  items: Item[];
+  position: string
+  items: Item[]
 }
 
 interface Item {
-  firstName: string;
-  lastName: string;
-  fullName: string;
-  birthPlace: BirthPlace;
-  college?: College;
-  position: Position;
-  debutYear?: number;
-  [key: string]: any;
+  firstName: string
+  lastName: string
+  fullName: string
+  birthPlace: BirthPlace
+  college?: College
+  position: Position
+  debutYear?: number
+  [key: string]: any
 }
 
 interface Position {
-  name: string;
-  [key: string]: any;
+  name: string
+  [key: string]: any
 }
 
 interface College {
-  name: string;
-  shortName: string;
-  [key: string]: any;
+  name: string
+  shortName: string
+  [key: string]: any
 }
 
 interface BirthPlace {
-  city?: string;
-  state?: string;
-  country?: string;
+  city?: string
+  state?: string
+  country?: string
 }
